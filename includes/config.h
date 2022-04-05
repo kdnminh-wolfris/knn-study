ca94eab4b3c62ee9600c0be153df95754c0bd871
kdnminh-wolfris <khaudangnhatminh@gmail.com>
2022-04-05 18:14:07 +0700

    add defined functions to config.h

--- includes/config.h
+++ includes/config.h
@@ -6,6 +6,10 @@
 #define MAX_NUMBER_OF_DIMENSIONS 50
 #define MAX_NUMBER_OF_NEIGHBOURS 100
 
+#define sqr(x) (x * x)
+#define intceildiv(x, y) ((x + y - 1) / y)
+#define block_cnt(x) intceildiv(x, MAX_THREADS)
+
 #define __DEBUG__
 
 #endif // __CONFIG__
